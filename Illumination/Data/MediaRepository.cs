c670206fa9a4ca944a733e331ce5885764a7f35d
unknown <Hoon@.(none)>
2010-08-01 16:43:53 -0700

    again...

---
+++ Illumination/Data/MediaRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework;
+
+namespace Illumination.Data
+{
+    public static class MediaRepository
+    {
+        public static Dictionary<string, Texture2D> Textures;
+        public static Dictionary<string, SoundEffect> SoundEffects;
+        public static Dictionary<string, Song> Songs;
+        public static Dictionary<string, SpriteFont> Fonts;
+
+        private static bool loaded = false;
+
+        public static void LoadAll(Game gameInstance)
+        {
+            Textures = new Dictionary<string, Texture2D>();
+            SoundEffects = new Dictionary<string, SoundEffect>();
+            Songs = new Dictionary<string, Song>();
+            Fonts = new Dictionary<string, SpriteFont>();
+
+            Textures.Add("GrassTile", gameInstance.Content.Load<Texture2D>("grasstile"));
+
+            loaded = true;
+        }
+
+        public static bool IsLoaded()
+        {
+            return loaded;
+        }
+    }
+}
---
+++ Illumination/Illumination.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using Illumination.Logic;
+using Illumination.Data;
+
+namespace Illumination
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Illumination : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        World world;
+
+        public Illumination()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            world = new World(50, 50, graphics.GraphicsDevice.Viewport.Width, graphics.GraphicsDevice.Viewport.Height);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            MediaRepository.LoadAll(this);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            spriteBatch.Begin();
+            world.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
