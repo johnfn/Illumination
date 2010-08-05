fa88e8d8fa4cb62223fabbe2265943cf54efbc05
David Wu <icebuster@gmail.com>
2010-08-04 20:54:02 -0700

    Added mouse handler (left mouse button only at this point) - tests are present in Illumination (commented out for now)

---
+++ Illumination/Logic/MouseHandler/MouseListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Illumination.Logic.MouseHandler {
+    public interface MouseListener {
+        void MouseClicked(MouseEvent evt);
+        void MousePressed(MouseEvent evt);
+        void MouseReleased(MouseEvent evt);
+
+        void MouseMoved(MouseEvent evt);
+        void MouseDragged(MouseEvent evt);
+    }
+}
