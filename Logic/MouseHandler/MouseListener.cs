774eb996222ae417ca4a26f7130e31417eb03470
johnfn <johnfn@gmail.com>
2010-08-08 14:26:18 -0700

    Initial commit to github

---
+++ Logic/MouseHandler/MouseListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Illumination.Logic.MouseHandler {
+    public interface MouseListener {
+
+
+        void MouseClicked(MouseEvent evt);
+        void MousePressed(MouseEvent evt);
+        void MouseReleased(MouseEvent evt);
+    }
+}
---
+++ WorldObjects/WorldObject.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Illumination.WorldObjects
+{
+    public abstract class WorldObject
+    {
+        Rectangle boundingBox;
+
+        public Rectangle BoundingBox 
+        {
+            get { return boundingBox; }
+            set { boundingBox = value; }
+        }
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
