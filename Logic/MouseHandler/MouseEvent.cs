d5aa24c88f93c8ef6a9423c27899fa288d7e7446
David Wu <icebuster@gmail.com>
2010-08-08 15:30:00 -0700

    Last restore (hopefully?)

---
+++ Logic/MouseHandler/MouseEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Illumination.Logic.MouseHandler {
+    public class MouseEvent {
+        private Point startLocation;
+        private Point currentLocation;
+
+        public Point StartLocation {
+            get { return startLocation; }
+        }
+
+        public Point CurrentLocation {
+            get { return currentLocation; }
+        }
+
+        public MouseEvent(Point startLocation, Point currentLocation) {
+            this.startLocation = startLocation;
+            this.currentLocation = currentLocation;
+        }
+
+        public MouseEvent(Point currentLocation) {
+            this.startLocation = this.currentLocation = currentLocation;
+        }
+    }
+}
---
+++ Utility/Geometry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Illumination.Utility {
+    static class Geometry {
+        public static double Distance(Point p1, Point p2) {
+            int dx = p1.X - p2.X;
+            int dy = p1.Y - p2.Y;
+
+            return Math.Sqrt(dx * dy + dy * dy);
+        }
+    }
+}
